cc92c1a83ceab92664600efa013fda4bf4fc9c38
Gabriel Furquim <gabriel.furquim@outlook.com>
2023-11-29 15:28:20 -0300

    feat: add locale and pipes for currency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+registerLocaleData(localePt)
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
